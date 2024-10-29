2c1f22e34a13843456049a11e4ffb3872a36e4ef
David Linca <alexlinca_21@yahoo.ro>
2024-10-29 10:13:29 +0200

    Structure mods.

---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,10 @@
+﻿using Linca_David_Lab2.Models;
+
+namespace Linca_David_Lab2.Models.ViewModels
+{
+    public class PublisherIndexData
+    {
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+    }
+}
